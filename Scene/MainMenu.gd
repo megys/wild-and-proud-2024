fe8aaa60883e78bb9e8c0008aa57d8b6b5a6a9bc
Manny-OnGit <manny.fluss@gmail.com>
2024-06-22 18:50:44 -0700

    created global audio slider, created main menu.

---
+++ Scene/MainMenu.gd
@@ -0,0 +1,21 @@
+extends Control
+
+
+@export_file("*.tscn")
+var file_path:String
+
+
+
+func _on_play_button_pressed() -> void:
+	get_tree().change_scene_to_file(file_path)
+
+
+func _on_credits_button_pressed() -> void:
+	$LazyCredits.visible = true
+	pass # Replace with function body.
+
+
+func _on_exit_credits_button_pressed() -> void:
+	$LazyCredits.visible = false
+	
+	pass # Replace with function body.
